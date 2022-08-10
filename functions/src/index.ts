4215563355ac16c9ed88c1f4f6c0a08dec97a6b6
LeaderbotX400 <eric@singerfamily.ca>
2022-08-10 10:04:52 -0700

    organize cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,140 +2,6 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 admin.initializeApp();
 
-interface Item {
-  name: string;
-  price: number;
-  stock: number;
-  available: boolean;
-  stats: {
-    totalSold: number;
-  };
-}
-
-export const addItem = functions.https.onCall(async (data, context) => {
-  if (context.app == undefined) {
-    throw new functions.https.HttpsError("permission-denied", "Unknown origin");
-  }
-  if (context.auth?.token.admin !== true) {
-    throw new functions.https.HttpsError(
-        "permission-denied",
-        "You must be an administator to do this"
-    );
-  }
-
-  console.log(data);
-  const item = {
-    name: data.name as string,
-    price: Number(data.price),
-    stock: Number(data.stock),
-    stats: {
-      totalSold: 0 as number,
-    },
-    available: data.available as boolean,
-  };
-  const batch = admin.firestore().batch();
-  batch.update(admin.firestore().doc("data/items"), {
-    items: admin.firestore.FieldValue.arrayUnion(item),
-  });
-
-  return batch.commit().catch((err) => {
-    throw new functions.https.HttpsError("internal", err.message);
-  });
-});
-
-export const deleteItem = functions.https.onCall(async (data, context) => {
-  if (context.app == undefined) {
-    throw new functions.https.HttpsError("permission-denied", "Unknown origin");
-  }
-  if (context.auth?.token.admin !== true) {
-    throw new functions.https.HttpsError(
-        "permission-denied",
-        "You must be an administator to do this"
-    );
-  }
-  const batch = admin.firestore().batch();
-  batch.update(admin.firestore().doc("data/items"), {
-    items: admin.firestore.FieldValue.arrayRemove(data.item),
-  });
-  return batch.commit().catch((err) => {
-    return err;
-  });
-});
-
-export const updateItem = functions.https.onCall(async (data, context) => {
-  if (context.app == undefined) {
-    throw new functions.https.HttpsError("permission-denied", "Unknown origin");
-  }
-  if (context.auth?.token.admin !== true) {
-    throw new functions.https.HttpsError(
-        "permission-denied",
-        "You must be an administator to do this"
-    );
-  }
-  const batch = admin.firestore().batch();
-  batch.update(admin.firestore().doc("data/items"), {
-    items: data.items,
-  });
-  return batch.commit().catch((err) => {
-    return err;
-  });
-});
-
-export const submitOrder = functions.https.onCall(async (data, context) => {
-  console.log(context.auth);
-  if (context.app == undefined) {
-    throw new functions.https.HttpsError("permission-denied", "Unknown origin");
-  }
-  const docRef = admin.firestore().collection("orders").doc();
-  const batch = admin.firestore().batch();
-  batch.set(docRef, {
-    completed: false as boolean,
-    items: data.items,
-    total: data.total as number,
-    created: admin.firestore.FieldValue.serverTimestamp(),
-    email: context.auth?.token.email as string,
-  });
-  // update item stats
-  await admin.firestore().runTransaction(async (t) => {
-    const doc = await t.get(admin.firestore().doc("data/items"));
-    const items = doc.data()?.items;
-    if (items == undefined || items.length == 0) {
-      throw new functions.https.HttpsError("internal", "No items found");
-    }
-    const newItems = items.map((item: Item) => {
-      const index = data.items.findIndex((i: Item) => i.name === item.name);
-      if (index !== -1) {
-        item.stock -= Number(data.items[index].quantity);
-        item.available = (item.stock > 0) as boolean;
-        item.stats.totalSold += Number(data.items[index].quantity);
-      }
-      return item;
-    });
-    batch.update(admin.firestore().doc("data/items"), {
-      items: newItems,
-    });
-  });
-  // update user stats
-  await admin.firestore().runTransaction(async (t) => {
-    const doc = await t.get(
-        admin.firestore().doc(`users/${context.auth?.uid}`)
-    );
-    const user = doc.data();
-    if (user == undefined) {
-      throw new functions.https.HttpsError("internal", "No user found");
-    }
-    batch.update(admin.firestore().doc(`users/${context.auth?.uid}`), {
-      stats: {
-        totalOrders: admin.firestore.FieldValue.increment(1),
-        totalSpent: Number(user.stats.totalSpent + data.total),
-      },
-    });
-  });
-  return batch.commit().catch((err) => {
-    throw new functions.https.HttpsError("internal", err);
-  });
-});
-
 export const newUser = functions.auth.user().onCreate(async (user) => {
   const batch = admin.firestore().batch();
   batch.set(admin.firestore().doc(`users/${user.uid}`), {
@@ -166,19 +32,19 @@ export const toggleAdmin = functions.https.onCall(async (data, context) => {
   }
   if (context.auth?.token.admin !== true) {
     throw new functions.https.HttpsError(
-        "permission-denied",
-        "You must be an administator to do this"
+      "permission-denied",
+      "You must be an administrator to do this"
     );
   }
   const user = await admin.auth().getUserByEmail(data.email);
   await admin
-      .auth()
-      .setCustomUserClaims(user.uid, {
-        admin: data.admin as boolean,
-      })
-      .catch((err) => {
-        throw new functions.https.HttpsError("internal", err.message);
-      });
+    .auth()
+    .setCustomUserClaims(user.uid, {
+      admin: data.admin as boolean,
+    })
+    .catch((err) => {
+      throw new functions.https.HttpsError("internal", err.message);
+    });
   const batch = admin.firestore().batch();
   batch.update(admin.firestore().doc(`users/${user.uid}`), {
     roles: {
@@ -196,8 +62,8 @@ export const deleteUser = functions.https.onCall(async (data, context) => {
   }
   if (context.auth?.token.dev !== true && context.auth?.token.admin !== true) {
     throw new functions.https.HttpsError(
-        "permission-denied",
-        "this is a dev only function"
+      "permission-denied",
+      "this is a dev only function"
     );
   }
   const user = await admin.auth().getUserByEmail(data.email);
@@ -205,10 +71,10 @@ export const deleteUser = functions.https.onCall(async (data, context) => {
   const batch = admin.firestore().batch();
   batch.delete(admin.firestore().doc(`users/${user.uid}`));
   const orders = await admin
-      .firestore()
-      .collection("orders")
-      .where("email", "==", data.email)
-      .get();
+    .firestore()
+    .collection("orders")
+    .where("email", "==", data.email)
+    .get();
   orders.forEach((doc) => {
     batch.delete(doc.ref);
   });
@@ -216,3 +82,8 @@ export const deleteUser = functions.https.onCall(async (data, context) => {
     return err;
   });
 });
+
+import * as orders from "./orders";
+import * as items from "./items";
+
+export { orders, items };
---
+++ functions/src/items.ts
@@ -0,0 +1,71 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+export const addItem = functions.https.onCall(async (data, context) => {
+  if (context.app == undefined) {
+    throw new functions.https.HttpsError("permission-denied", "Unknown origin");
+  }
+  if (context.auth?.token.admin !== true) {
+    throw new functions.https.HttpsError(
+        "permission-denied",
+        "You must be an administrator to do this"
+    );
+  }
+
+  console.log(data);
+  const item = {
+    name: data.name as string,
+    price: Number(data.price),
+    stock: Number(data.stock),
+    stats: {
+      totalSold: 0 as number,
+    },
+    available: data.available as boolean,
+  };
+  const batch = admin.firestore().batch();
+  batch.update(admin.firestore().doc("data/items"), {
+    items: admin.firestore.FieldValue.arrayUnion(item),
+  });
+
+  return batch.commit().catch((err) => {
+    throw new functions.https.HttpsError("internal", err.message);
+  });
+});
+
+export const deleteItem = functions.https.onCall(async (data, context) => {
+  if (context.app == undefined) {
+    throw new functions.https.HttpsError("permission-denied", "Unknown origin");
+  }
+  if (context.auth?.token.admin !== true) {
+    throw new functions.https.HttpsError(
+        "permission-denied",
+        "You must be an administrator to do this"
+    );
+  }
+  const batch = admin.firestore().batch();
+  batch.update(admin.firestore().doc("data/items"), {
+    items: admin.firestore.FieldValue.arrayRemove(data.item),
+  });
+  return batch.commit().catch((err) => {
+    return err;
+  });
+});
+
+export const updateItem = functions.https.onCall(async (data, context) => {
+  if (context.app == undefined) {
+    throw new functions.https.HttpsError("permission-denied", "Unknown origin");
+  }
+  if (context.auth?.token.admin !== true) {
+    throw new functions.https.HttpsError(
+        "permission-denied",
+        "You must be an administrator to do this"
+    );
+  }
+  const batch = admin.firestore().batch();
+  batch.update(admin.firestore().doc("data/items"), {
+    items: data.items,
+  });
+  return batch.commit().catch((err) => {
+    return err;
+  });
+});
